2a01f952be999af596286cc9fb681879458b0317
mstrYoda <freelancemuhendis@gmail.com>
2019-04-12 23:23:30 +0300

    added memoization example

---
+++ src/main/java/com/yoda/MethodMemoization.java
@@ -0,0 +1,17 @@
+package com.yoda;
+
+import io.vavr.Function1;
+
+public class MethodMemoization {
+    public void run() {
+        Function1 memoizedFunc = Function1.of(MethodMemoization::square).memoized();
+
+        memoizedFunc.apply(5);
+        memoizedFunc.apply(5);
+    }
+
+    public static int square(int num) {
+        System.out.println("factorial calculated for : " + num);
+        return num * num;
+    }
+}
